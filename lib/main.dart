b997930c31a8e020392ac84d6f991ff348da21aa
Your Name <you@example.com>
2019-03-21 17:08:15 +0800

    'page_view'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttershuachi/pages/animation.dart';
 import 'package:fluttershuachi/pages/home.dart';
+import 'package:fluttershuachi/pages/page_view.dart';
 import 'package:fluttershuachi/pages/share_demo.dart';
 import 'package:fluttershuachi/pages/test_status_demo.dart';
 
@@ -13,12 +14,13 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/shart',
+      initialRoute: '/pageview',
       routes: {
         '/': (context) => Home(),
         '/animation': (context) => AnimationDemo(),
         '/teststatus': (context) => TestStatusDemo(),
         '/shart': (context) => ShartDemo(),
+        '/pageview': (context) => ViewDemo(),
       },
       theme: ThemeData(
         highlightColor: Color.fromARGB(255, 255, 255, 255)
