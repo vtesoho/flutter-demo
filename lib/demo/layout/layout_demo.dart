78a6710465a7785925bd09d0301622d3eef22b20
Your Name <vtesoho@163.com>
2019-03-24 20:10:53 +0800

    'constrainedBox'

--- lib/demo/layout/layout_demo.dart
+++ lib/demo/layout/layout_demo.dart
@@ -1,88 +1,118 @@
 import 'package:flutter/material.dart';
 
-class MyPage extends StatefulWidget {
-  @override
-  _MyPageState createState() => _MyPageState();
-}
-
-class _MyPageState extends State<MyPage> {
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('data'),
+        title: Text('layout_demo'),
       ),
-      body: Container(
-        height:500,
-        width:300,
-        decoration: BoxDecoration(
-          image:DecorationImage(
-            image:NetworkImage('http://pic19.nipic.com/20120321/3001435_141555714185_2.jpg'),
-            alignment: Alignment.bottomCenter,
-            // repeat: ImageRepeat.repeatY
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.lightGreenAccent, BlendMode.plus)
-          )
-        ),
-        child: Row(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            // Chip(
-            //   label: Text('string', style: new TextStyle(fontSize: 16.0)),
-            //   avatar: Icon(Icons.search),
-            //   deleteIcon: Icon(Icons.clear, color: Colors.black12),
-            //   labelPadding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-            //   onDeleted: () {
-            //     print('delete');
-            //   },
-            // ),
-            Container(
-              child: Icon(Icons.pool,size:32,color: Colors.white),
-              padding: EdgeInsets.all(10),
-              width: 90,
-              height:90,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                border: Border.all(
-                  color: Colors.red,
-                ),
-                // borderRadius: BorderRadius.circular(16.0),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(6.0, 7.0),
-                    color: Color.fromRGBO(125, 125, 125, 0.8),
-                    blurRadius: 10.0,
-                    spreadRadius: 5,
-                  ),
-                  BoxShadow(
-                    offset: Offset(-5, 0),
-                    color:Colors.red,
-                    blurRadius: 1.0,
-                    spreadRadius: 1,
-                  )
-                ],
-                shape: BoxShape.circle,  //不能与borderRadius共用
-                // gradient: RadialGradient(
-                //   colors: [
-                //     Color.fromRGBO(10, 255, 10, 1.0),
-                //     Color.fromRGBO(255, 10, 255, 1.0),
-                //   ]
-                // )
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromRGBO(10, 255, 10, 1.0),
-                    Color.fromRGBO(255, 10, 255, 1.0),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                )
-              ),
-            )
+            ConstrainedBoxDemo(),
           ],
-        ),
+        )
       )
+      
+      
+    );
+  }
+}
+
+
+class ConstrainedBoxDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        minHeight: 200.0,
+        maxWidth: 200.0,
+      ),
+      child: Container(
+        color: Colors.yellow,
+      ),
+    );
+  }
+}
+
+
+class AspectRatioDemo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+} 
+
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight,
+      children: <Widget>[
+        SizedBox(
+          width:300,
+          height:300,
+          child: Container(
+            alignment: Alignment(-1.0, 0),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Icon(Icons.ac_unit, color: Colors.white, size:32.0),
+          ),
+        ),
+        SizedBox(height: 10,),
+        SizedBox(
+          width:100,
+          height:100,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                colors: [
+                  Colors.blue,
+                  Colors.red,
+                ]
+              )
+            ),
+            child: Icon(Icons.ac_unit, color: Colors.white, size:32.0),
+          ),
+        ),
+        Positioned(
+          right: 20,
+          top: 222,
+          child: Icon(Icons.ac_unit, color: Colors.white, size:32.0),
+        )
+      ],
+    );
+  }
+}
+
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon, {this.size = 32.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(icon,size:size, color: Colors.white),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(10, 100, 255, 1.0)
     );
   }
 }
