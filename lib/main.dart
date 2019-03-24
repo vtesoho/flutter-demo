9738ddacbf7e6f0cf660d39b8082216db5763e02
Your Name <you@example.com>
2019-03-24 18:49:07 +0800

    'boxdecoration'

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershuachi/demo/state/state_management_demo.dart';
 import 'package:fluttershuachi/pages/animation.dart';
 import 'package:fluttershuachi/pages/home.dart';
+import 'package:fluttershuachi/pages/my_page.dart';
 import 'package:fluttershuachi/pages/page_view.dart';
 import 'package:fluttershuachi/pages/share_demo.dart';
 import 'package:fluttershuachi/pages/test_status_demo.dart';
@@ -14,13 +16,15 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/pageview',
+      initialRoute: '/mypage',
       routes: {
         '/': (context) => Home(),
         '/animation': (context) => AnimationDemo(),
         '/teststatus': (context) => TestStatusDemo(),
         '/shart': (context) => ShartDemo(),
         '/pageview': (context) => ViewDemo(),
+        '/state-management': (context) => StateManagementDemo(),
+        '/mypage': (context) => MyPage(),
       },
       theme: ThemeData(
         highlightColor: Color.fromARGB(255, 255, 255, 255)
