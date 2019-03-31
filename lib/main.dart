a7f13462d86adbeb194b30253c879b9ebafb1107
Your Name <vtesoho@163.com>
2019-03-31 19:45:06 +0800

    'sliver'

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fluttershuachi/demo/imagedown/dioDemo/dioDemo.dart';
 import 'package:fluttershuachi/demo/imagedown/image_picker_saver.dart';
 import 'package:fluttershuachi/demo/imagedown/imgdown/imgdown.dart';
 import 'package:fluttershuachi/demo/layout/layout_demo.dart';
+import 'package:fluttershuachi/demo/sliver/slive.dart';
 import 'package:fluttershuachi/demo/state/state_management_demo.dart';
 import 'package:fluttershuachi/demo/view/pageview.dart';
 import 'package:fluttershuachi/pages/animation.dart';
@@ -21,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/pageview',
+      initialRoute: '/sliver',
       routes: {
         '/': (context) => Home(),
         '/animation': (context) => AnimationDemo(),
@@ -31,6 +32,8 @@ class App extends StatelessWidget {
         '/mypage': (context) => MyPage(),
         '/layout': (context) => LayoutDemo(),
         '/pageview': (context) => Pageview(),
+        '/sliver': (context) => SliverDemo(),
+        
         
         // '/dio': (context) => dioDemo(),
         // '/imgsaver': (context) => ImgSaver(),
