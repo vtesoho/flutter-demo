01213beaca65b89d2cb8bde6817b88b0e1c4b19b
vtesoho <vtesoho@163.com>
2019-05-11 17:38:20 +0800

    websocker

--- lib/demo/websocker/websockerdemo.dart
+++ lib/demo/websocker/websockerdemo.dart
@@ -1,19 +1,72 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 
+/* 
+增加了一个server服务，用node跑起来连接测试用。
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({ port: 8181 });
+wss.on('connection', function (ws) {
+  console.log('client connected');
+  var sendStockUpdates = function (ws,index) {
+
+    var stocksObj = {
+      'aa': 22,
+      'bb': index,
+    }
+    if (ws.readyState == 1) {
+      if (stocksObj.length !== 0) {
+        ws.send(JSON.stringify(stocksObj));//需要将对象转成字符串。WebSocket只支持文本和二进制数据
+        console.log("更新", JSON.stringify(stocksObj));
+      }
+    }
+  }
+  var index = 0;
+  var clientStockUpdater = setInterval(function () {
+    sendStockUpdates(ws,index);
+    index++;
+  }, 1000);
+  ws.on('message', function (message) {
+    console.log(message);
+    sendStockUpdates(ws);
+  });
+});
+*/
+
 class WebSockerDemo extends StatefulWidget {
   WebSockerDemo({Key key}) : super(key: key);
-  final WebSocketChannel channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
+  final WebSocketChannel channel = IOWebSocketChannel.connect('ws://192.168.9.55:8181');
+  
+  
   _WebSockerDemoState createState() => _WebSockerDemoState();
 }
 
 class _WebSockerDemoState extends State<WebSockerDemo> {
   TextEditingController _controller = new TextEditingController();
   
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // widget.channel.stream.listen((onData){
+    //   print('onData');
+    // });
+  }
+  analyzeData(data) {
+    if(data == null) return;
+    var returnobject = json.decode(data);
+    // print(returnobject);
+    if(returnobject['aa'] != ''){
+      print('aa start ${returnobject['aa']}');
+    }
+    if(returnobject['bb'] != ''){
+      print('bb start ${returnobject['bb']}');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -34,9 +87,12 @@ class _WebSockerDemoState extends State<WebSockerDemo> {
             new StreamBuilder(
               stream: widget.channel.stream,
               builder: (context, snapshot) {
+                // print('${snapshot.hasData}');
+                // print('${snapshot.connectionState}');
+                analyzeData(snapshot.data);
                 return new Padding(
                   padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: new Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
                 );
               },
             )
@@ -61,5 +117,10 @@ class _WebSockerDemoState extends State<WebSockerDemo> {
   void dispose() {
     widget.channel.sink.close();
     super.dispose();
+    
   }
 }
+
+
+
+
