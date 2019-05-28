033e48b9a092a54c7ce8dbfab6e62f67a14c35f3
vtesoho <vtesoho@163.com>
2019-05-28 14:53:14 +0800

    toastNotifi

--- lib/demo/toast/toastDemo.dart
+++ lib/demo/toast/toastDemo.dart
@@ -1,151 +1,57 @@
 import 'package:flutter/material.dart';
 
-class ToastView {
-  OverlayEntry overlayEntry;
-  OverlayState overlayState;
-  bool dismissed = false;
-
-  _show() async {
-    overlayState.insert(overlayEntry);
-    await Future.delayed(Duration(milliseconds: 3500));
-    // this.dismiss();
-  }
-
-  dismiss() async {
-    if (dismissed) {
-      return;
-    }
-    this.dismissed = true;
-    overlayEntry?.remove();
-  }
-}
-
-
-class Toast {
+class ToastNotifi {
   BuildContext context;
+  OverlayState overlayState;
+  OverlayEntry overlayEntry;
+  factory ToastNotifi({contextData}) => _getInstance(contextData);
 
-  factory Toast({contextData}) => _getInstance(contextData);
-
-  static Toast _instance;
-  Toast._internal(contextData) {
-    print('contextData 初始化 -- $contextData');
+  static ToastNotifi _instance;
+  ToastNotifi._internal(contextData) {
     context = contextData;
+    overlayState = Overlay.of(context);
   }
-  static Toast _getInstance(contextData){
+  static ToastNotifi _getInstance(contextData) {
     if (_instance == null) {
-      print('初始化没有？$contextData');
-      _instance = Toast._internal(contextData);
+      _instance = ToastNotifi._internal(contextData);
     }
     return _instance;
   }
 
-
-  show() {
-    OverlayState overlayState = Overlay.of(context);
-    OverlayEntry overlayEntry;
+  show({Widget child}) {
+    if (overlayEntry != null) {
+      overlayEntry.remove();
+    }
     overlayEntry = OverlayEntry(builder: (context) {
-      void colse (){
-        overlayEntry.remove();
-      }
-      return NotifiA(colse:colse);
+      return NotifiBaseWidget(homeContext: context, child: child);
     });
-    var toastView = ToastView();
-    toastView.overlayEntry = overlayEntry;
-    toastView.overlayState = overlayState;
-    toastView._show();
+    overlayState.insert(overlayEntry);
   }
 
-
-
+  dismiss() async {
+    overlayEntry.remove();
+    overlayEntry = null;
+  }
 }
 
-class NotifiA extends StatelessWidget {
-  var colse;
-  NotifiA({Key key,this.colse}) : super(key: key);
+class NotifiBaseWidget extends StatelessWidget {
+  final colse;
+  final BuildContext homeContext;
+  final Widget child;
+  NotifiBaseWidget({Key key, this.colse, this.homeContext, this.child})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
+    return GestureDetector(
+      child: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(homeContext).padding.top),
         child: Column(
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              // 双精度（`double`）类的无穷（`infinity`）常量，最大宽度。
-              width: double.infinity,
-              height: 85.0,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white.withOpacity(0.95),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xff000000).withOpacity(0.1),
-                        blurRadius: 5.0,
-                      ),
-                    ]),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 8.0, right: 8.0, top: 5.0, bottom: 5.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            '刷吃',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              colse();
-                              print('aaaaaaaaaaaaaaaaaaa');
-                            },
-                            child: Text(
-                              '关闭',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                      child: Text(
-                        '111111111111',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        '222222222222222222222',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            child,
           ],
         ),
-      );
+      ),
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,7 @@ import '../store/app/AppState.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ToastNotifi(contextData: context);
     // Timer(Duration(seconds: 5), () {
     //   print('这是一个SnackBar');
     //   // openTopReminder(context,'这是一个测试');
@@ -92,8 +93,7 @@ class Home extends StatelessWidget {
             
             RaisedButton(
               onPressed: () {
-                Toast(contextData: context);
-                Toast().show();
+                ToastNotifi().show(child: toastWidget());
               },
               child: Text('Toast Test'),
             ),
@@ -112,5 +112,88 @@ class Home extends StatelessWidget {
         ),
       ),
     );
+  }
+
+
+  Widget toastWidget (){
+    return Container(
+              width: double.infinity,
+              height: 85.0,
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Material(
+                color: Colors.white.withOpacity(0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.white.withOpacity(0.95),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xff000000).withOpacity(0.1),
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 8.0, right: 8.0, top: 5.0, bottom: 5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              '刷吃',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                // colse();
+                                ToastNotifi().dismiss();
+                                print('aaaaaaaaaaaaaaaaaaa');
+                              },
+                              child: Text(
+                                '关闭',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Text(
+                          '111111111111',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          '222222222222222222222',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
   }
 }
