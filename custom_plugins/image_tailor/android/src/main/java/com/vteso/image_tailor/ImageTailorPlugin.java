a0ba46cb30d1b830c799d85da655b5c63d9e3267
vtesoho <vtesoho@163.com>
2019-07-18 17:30:29 +0800

    imagetailor

--- custom_plugins/image_tailor/android/src/main/java/com/vteso/image_tailor/ImageTailorPlugin.java
+++ custom_plugins/image_tailor/android/src/main/java/com/vteso/image_tailor/ImageTailorPlugin.java
@@ -1,25 +1,129 @@
 package com.vteso.image_tailor;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.tools.PictureFileUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** ImageTailorPlugin */
-public class ImageTailorPlugin implements MethodCallHandler {
+public class ImageTailorPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+
+
+  private Activity activity;
+  private Registrar registrar;
+  private List<LocalMedia> selectList = new ArrayList<>();
+  private Result result;
+
+  public ImageTailorPlugin(Registrar registrar) {
+    this.registrar = registrar;
+    this.activity = registrar.activity();
+  }
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "image_tailor");
-    channel.setMethodCallHandler(new ImageTailorPlugin());
+    channel.setMethodCallHandler(new ImageTailorPlugin(registrar));
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
+    this.result = result;
+    registrar.addActivityResultListener(this);
+    if (call.method.equals("selectImg")) {
+      int maxNum = call.argument("maxImages");
+      int x = call.argument("width");
+      int y = call.argument("height");
+      startChooseImage(x,y,maxNum);
+    } else if(call.method.equals("clearCache")) {
+      PictureFileUtils.deleteCacheDirFile(activity);
+    } else{
       result.notImplemented();
     }
   }
+
+
+
+  /**
+   * 选择图片并裁剪
+   * @param x 横向比例
+   * @param y 宽向比例
+   * @param maxNum 最大数
+   */
+  private void startChooseImage(int x,int y, int maxNum){
+    // 进入相册
+    PictureSelector.create(activity)
+            .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+            .theme(R.style.picture_shuchi_style)
+            .maxSelectNum(maxNum)// 最大图片选择数量 int
+            .minSelectNum(1)// 最小选择数量 int
+            .imageSpanCount(4)// 每行显示个数 int
+            .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选 PictureConfig.MULTIPLE or PictureConfig.SINGLE
+            .previewImage(true)// 是否可预览图片 true or false
+            .isCamera(false)// 是否显示拍照按钮 true or false
+            .imageFormat(PictureMimeType.PNG)// 拍照保存图片格式后缀,默认jpeg
+            .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+            .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+            .enableCrop(true)// 是否裁剪 true or false
+            .compress(true)// 是否压缩 true or false
+            .withAspectRatio(x,y)// int 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+            .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示 true or false
+            .isGif(false)// 是否显示gif图片 true or false
+            .freeStyleCropEnabled(false)// 裁剪框是否可拖拽 true or false
+            .circleDimmedLayer(false)// 是否圆形裁剪 true or false
+            .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false   true or false
+            .showCropGrid(true)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false    true or false
+            .openClickSound(false)// 是否开启点击声音 true or false
+            .previewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
+            .minimumCompressSize(100)// 小于100kb的图片不压缩
+            .synOrAsy(true)//同步true或异步false 压缩 默认同步
+            .cropWH(x,y)// 裁剪宽高比，设置如果大于图片本身宽高则无效 int
+            .rotateEnabled(false) // 裁剪是否可旋转图片 true or false
+            .scaleEnabled(true)// 裁剪是否可放大缩小图片 true or false
+            .isDragFrame(true)// 是否可拖动裁剪框(固定)
+            .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+  }
+
+  @Override
+  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+    List<String> resultList = new ArrayList<>();
+    if (data == null) {
+      result.success(resultList);
+      return true;
+    }
+    if (resultCode == Activity.RESULT_OK) {
+      switch (requestCode) {
+        case PictureConfig.CHOOSE_REQUEST:
+          // 图片选择结果回调
+          selectList = PictureSelector.obtainMultipleResult(data);
+          // 例如 LocalMedia 里面返回三种path
+          // 1.media.getPath(); 为原图path
+          // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+          // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+          // 如果裁剪并压缩了，已取压缩路径为准，因为是先裁剪后压缩的
+          for(int i = 0; i< selectList.size();i++) {
+            System.out.println(">>>>>>>>>>>>>"+selectList.get(i));
+            resultList.add(selectList.get(i).getCutPath());
+          }
+          break;
+      }
+    }
+    result.success(resultList);
+    return true;
+  }
+
+
 }
--- custom_plugins/image_tailor/lib/image_tailor.dart
+++ custom_plugins/image_tailor/lib/image_tailor.dart
@@ -1,23 +1,27 @@
 import 'dart:async';
-
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
+import 'dart:convert';
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart' as crypto;
 
 class ImageTailor {
   static const MethodChannel _channel =
       const MethodChannel('image_tailor');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  // static Future<String> get platformVersion async {
+  //   final String version = await _channel.invokeMethod('getPlatformVersion');
+  //   return version;
+  // }
 
   static get finish async {
     await _channel.invokeMethod('finish');
   }
 
 
-  static Future<dynamic> selectImg({
+  Future<dynamic> selectImg({
     @required int maxImages,
     int width = 800,
     int height = 600,
@@ -29,11 +33,14 @@ class ImageTailor {
     bool closeOnLimitReached = false,
   }) async {
     assert(maxImages != null);
-
     if (maxImages != null && maxImages < 0) {
       throw new ArgumentError.value(maxImages, 'maxImages cannot be negative');
     }
-
+    var path = await _localPath;
+    var filePath;
+    var time= new DateTime.now().microsecondsSinceEpoch;
+    var picList = [];
+    var filename;
     final dynamic items =
         await _channel.invokeMethod('selectImg', <String, dynamic>{
       "onlySquare": onlySquare,
@@ -48,7 +55,33 @@ class ImageTailor {
       "text": text,
       "closeOnLimitReached": closeOnLimitReached,
     });
-
+    if(Platform.isIOS){
+      
+      for (var i = 0; i < items.length; i++) {
+        
+        filename = generateMd5('${time}${i}');
+        filePath = '$path/tmp/${filename}';
+        print('------------${filePath}');
+        new File(filePath).writeAsBytesSync(items[i]);
+        picList.add(filePath);
+      }   
+    }
+    print("--------------------${picList}");
     return items;
   }
+
+  Future<String> get _localPath async {
+    final directory = await getTemporaryDirectory();
+    return directory.path;
+  }
+
+
+  generateMd5(String data) {
+    var content = new Utf8Encoder().convert(data);
+    var md5 = crypto.md5;
+    var digest = md5.convert(content);
+    return hex.encode(digest.bytes);
+  } 
+
+
 }
